42d099cea5f692ed3fbdd1ce6088aff3d4746ee6
Elisha Bemjamin <malishben360@gmail.com>
2022-09-09 15:46:15 -0700

    Comforming to Betty coding style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+
 /**
   * main - Returns 0 and prints a text using printf 
   */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
+
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
+
 /**
   * main - returns 0 and prints the various types on the computer ii is compiled and run on
   */
 int main(void)
 {
-	printf("Size of a char: %d bytes(s)\n", (int) sizeof(char));
-	printf("Size of an int: %d bytes(s)\n", (int) sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	printf("Size of a char: %d bytes(s)", (int) sizeof(char));
+	printf("Size of an int: %d bytes(s)", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)", (int) sizeof(float));
 	return (0);
 }
+

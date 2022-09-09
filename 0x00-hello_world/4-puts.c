4b534b3ae78e34978469d92ed9cda183902418d5
Elisha Bemjamin <malishben360@gmail.com>
2022-09-09 14:56:34 -0700

    Task 4 fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+
 /**
-  *main - returns 0 and prints text using puts method
+  * main - Prints text using puts() method
+  * return: "Programming is like building a multilingual puzzle
   */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building
+			a multilingual puzzle\n");
 	return (0);
 }

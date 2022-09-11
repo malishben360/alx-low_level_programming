faaf058bbecc146af877ba485d8be593ab708acf
Elisha Bemjamin <malishben360@gmail.com>
2022-09-11 05:14:07 -0700

    Task 6 bug fix 1.1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
   * main - Entry point
   *
-  * Description: Prints the size of various types on the computer it is compiled and run
+  * Description: Prints the size of various types
   *
   * Return: Always 0 (Success)
   */

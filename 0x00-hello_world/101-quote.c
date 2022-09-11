40b08197f1a9c3710ef1f631990c666599b6f494
Elisha Bemjamin <malishben360@gmail.com>
2022-09-11 08:15:48 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Description: Print a text using function other than printf
+  *
+  * Return: Alway 1 (Fail)
+  */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

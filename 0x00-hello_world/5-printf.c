06782a411d90301dd94e0157316d06e1201d6004
Elisha Bemjamin <malishben360@gmail.com>
2022-09-11 04:24:18 -0700

    Task 5 bug fix 1.0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+/**
+  * main - Entry point
+  *
+  * Description: Prints a text using the printf() function
+  *
+  * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");

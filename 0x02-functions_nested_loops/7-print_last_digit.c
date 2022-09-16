eba40dd9d7c8fd3554b17afa6b00c3840dd08ec1
Elisha Bemjamin <malishben360@gmail.com>
2022-09-16 15:02:02 -0700

    Task 7 0x02 LLP

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+  * print_last_digit - Compute the last digit
+  *
+  * Description: Compute the last digit and prints it out
+  *
+  * @n: The int to compute its last digit
+  *
+  * Return: Alway the last digit of n
+  */
+int print_last_digit(int n)
+{
+	int last_digit, abs;
+
+	abs = (n < 0)? (n * -1) : n;
+	last_digit = abs % 10;
+	_putchar(last_digit + '0');
+	return (last_digit);
+}

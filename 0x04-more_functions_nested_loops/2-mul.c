d61d2e91db881e3618779e572e69fa337191e63d
Elisha Bemjamin <malishben360@gmail.com>
2022-09-17 13:49:56 -0700

    Task 2 0x04 LLP

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * mul - perform multiplication
+  *
+  * Description: Multiply n1 n2 times
+  *
+  * @n1: Multiplicant
+  *
+  * @n2: Multiplier
+  *
+  * Return: Always the multiple of n1 n2 times
+  */
+int mul(int n1, int n2)
+{
+	int result;
+
+	result = n1 * n2;
+	return (result);
+}

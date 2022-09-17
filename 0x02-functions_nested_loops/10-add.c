7d43be06ef9c6dd3ffefadfdc562ededbf3c8bcf
Elisha Bemjamin <malishben360@gmail.com>
2022-09-17 06:25:53 -0700

    Task 10 0x02 LLP

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+  * add - Sum up two numbers
+  *
+  * Description: Add the two arguments up
+  *
+  * Return: Always the sum of the two arguments
+  */
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);
+}

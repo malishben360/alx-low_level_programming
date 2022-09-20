197e699ab1969a1965e7fbc62bf6c2d8d38c36ea
Elisha Bemjamin <malishben360@gmail.com>
2022-09-19 19:22:54 -0700

    Task 4 0x05 LLP

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+  * print_rev - print a string in reverse form
+  *
+  * @s: The string to be printed in reverse form
+  *
+  * Description: Print a string in reverse form using _putchar
+  *
+  * Return: nothing.
+  */
+void print_rev(char *s)
+{
+	int len;
+	int i;
+	char c;
+
+	len = _strlen(s);
+	for (i = len; i >= 0; i--)
+	{
+		c = *(s + i);
+		_putchar(c);
+	}
+	_putchar(10);
+}
+
+/**
+  * _strlen - compute the length of a string
+  *
+  * @str: The string whose length is to be computed
+  *
+  * Description: Compute the length of a string and return it
+  *
+  * Return: Always the length of a string
+  */
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != 0)
+		i++;
+	return (i);
+}

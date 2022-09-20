3a1c61eec29d533eb38a3b9ba153eb1e7f0079cd
Elisha Bemjamin <malishben360@gmail.com>
2022-09-20 16:13:16 -0700

    Task 6 bug fix 1.0 0x05 LLP

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,14 +14,15 @@ void puts2(char *str)
 	int i;
 
 	if (*str != '\0')
-		_putchar(*str);
-
-	i = 1;
-	while (*(str + i) != '\0')
 	{
-		if ((i % 2) == 0)
-			_putchar(*(str + i));
-		i++;
+		_putchar(*str);
+		i = 1;
+		while (*(str + i) != '\0')
+		{
+			if ((i % 2) == 0)
+				_putchar(*(str + i));
+			i++;
+		}
 	}
 	_putchar(10);
 }

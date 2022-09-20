37201b01a8b1bf9c2509aed12fa0c218be91bbb3
Elisha Bemjamin <malishben360@gmail.com>
2022-09-20 15:55:55 -0700

    Task 6 0x05 LL

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * puts2 - print characters of a string
+  *
+  * @str: The string whoes characters is printed
+  *
+  * Description: Print the first element and any other characters
+  *
+  * Return: nothing.
+  */
+void puts2(char *str)
+{
+	int i;
+
+	_putchar(*str);
+	i = 1;
+	while (*(str + i) != '\0')
+	{
+		if ((i % 2) == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+	_putchar(10);
+}

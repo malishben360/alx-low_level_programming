08be30da3bacc07d2a50dfc3ed4e0ae5e863dc71
Elisha Bemjamin <malishben360@gmail.com>
2022-09-19 17:12:56 -0700

    Task 0 0x05 LLP

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - Set the value of the memory location *n to 98
+  *
+  * @n: The memory location to be set to 98
+  *
+  * Description: Change the value stored in a memory to 98
+  *
+  * Return: nothing
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

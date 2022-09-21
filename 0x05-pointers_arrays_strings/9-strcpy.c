2f9ce6f7e4aee43242facbec4412a897f75558f9
Elisha Bemjamin <malishben360@gmail.com>
2022-09-20 18:59:20 -0700

    Task 9 0x05 LLP

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _strcpy - copy the content of a string
+  *
+  * @dest: The new location
+  *
+  * @src: The original location
+  *
+  * Description: copy string from src to dest
+  *
+  * Return: The address of the destination.
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	index = 0;
+	while (*(src + index) != '\0')
+	{
+		dest[index] = *(src + index);
+		index++;
+	}
+	return (dest);
+}

ae068ccf24e21ec10eb75a503d7a9b69fff998bf
Elisha Bemjamin <malishben360@gmail.com>
2022-09-23 13:48:25 -0700

    Task 10 bug fix 1.0 0x05 LLP

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -26,14 +26,17 @@ int _atoi(char *str)
 			sign = -1;
 		}
 
-		while (str[i] >= '0' && str[i] <= '9' && str[i] != '\0')
+		while (str[i] != '\0')
 		{
-			num = num * 10 + str[i] - '0';
+			if (str[i] >= 48 && str[i] <= 57)
+			{
+				num = num * 10 + str[i] - '0';
+			}
 			i++;
 		}
 
 		num = num * sign;
-		return ((int) num);
+		return (num);
 	}
 	else
 	{

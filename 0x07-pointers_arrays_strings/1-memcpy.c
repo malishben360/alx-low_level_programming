02ef9c024a1faedf63a2c282b2c29f2332bf5c17
Elisha Bemjamin <malishben360@gmail.com>
2022-09-26 17:39:34 -0700

    Task 1 0x07 LLP

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _memcpy - copy the memory of an area
+  *
+  * @dest: the destination to be copied to
+  * @src: the source to be copied from
+  * @n: the number of bytes to be copied
+  *
+  * Description: Copy n bytes from src to dest and returned dest
+  *
+  * Return: Always the address dest
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}

100252640d66ead2efd9e8afcc82c2afffe3b703
Elisha Bemjamin <malishben360@gmail.com>
2022-09-29 10:42:09 -0700

    Task 2 bug fix 1.1 0x07 LLP

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
   * _strchr - find the address of the first currence of a character
@@ -20,5 +19,5 @@ char *_strchr(char *s, char c)
 			return (s);
 		s++;
 	}
-	return (NULL);
+	return ('\0');
 }

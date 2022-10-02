48232f8c807667088f371566d5e55fff1f46d2f0
Elisha Bemjamin <malishben360@gmail.com>
2022-10-02 04:09:34 -0700

    Task 3 0x0A LLP

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  *
+  * @argc: the number of arguments passed to main
+  * @argv: the array of arguments passed to main
+  *
+  * Description: compute the multiplication of two
+  * integers passed as arguments
+  *
+  * Return: Always multiplication, and 1 (Failure).
+  */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (!(argc < 3))
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+
+		return (result);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}

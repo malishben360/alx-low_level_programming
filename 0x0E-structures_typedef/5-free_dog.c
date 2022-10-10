ec75acfcc393b6f000a7acf6b3317cd21f7766ce
Elisha Bemjamin <malishben360@gmail.com>
2022-10-10 11:36:05 -0700

    Task 5 0x0E LLP

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * free_dog - frees the memory allocated to dog type
+  * @d: the memory pointer to be freed
+  *
+  * Return: nothing.
+  */
+void free_dog(dog_t *d)
+{
+	free(d);
+}

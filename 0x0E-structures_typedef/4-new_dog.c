ac2745bd52cf72fb744838bc271ecaa4703817c7
Elisha Bemjamin <malishben360@gmail.com>
2022-10-10 11:14:31 -0700

    Task 4 0x0E LLP

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+  * new_dog - creates an instance of the dog type
+  * @name: the name of the new dog
+  * @age: the new dog's age
+  * @owner: the owner of the new dog
+  *
+  * Return: Always pointer to the new dog, else NULL
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr;
+
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+
+	return (ptr);
+}

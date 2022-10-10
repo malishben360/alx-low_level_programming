38ae3872ff57629e4e506aa3567bd578ad50463f
Elisha Bemjamin <malishben360@gmail.com>
2022-10-10 02:36:38 -0700

    Task 2 0x0D LLP

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - The entry point
+  *
+  * Desceiption: prints the name of the curent file
+  *
+  * Return: Always 0 (Success).
+  */
+int main(void)
+{
+	printf(__FILE__ "\n");
+
+	return (0);
+}

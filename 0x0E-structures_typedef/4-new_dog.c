a43e140aa1a4f85a41226b9343e07812cdf6396c
Elisha Bemjamin <malishben360@gmail.com>
2022-10-11 08:25:10 -0700

    Task 4 0x0E LLP

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,14 +11,16 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr;
-
-	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
+	dog_t *ptr = malloc(sizeof(dog_t));
+	dog_t *ptr_cp = malloc(sizeof(dog_t));
+	if (ptr == NULL || ptr_cp == NULL)
 		return (NULL);
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
+
+	ptr_cp->name = name;
+	ptr_cp->owner = owner;
 
 	return (ptr);
 }

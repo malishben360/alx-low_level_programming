12792e34e0130ad9466fc6ef7b462a64f7a2241b
Elisha Bemjamin <malishben360@gmail.com>
2022-10-11 12:14:52 -0700

    Task 4 0x0E LLP

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
 
+char *cp(char *str);
+
 /**
   * new_dog - creates an instance of the dog type
   * @name: the name of the new dog
@@ -11,16 +13,49 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr = malloc(sizeof(dog_t));
-	dog_t *ptr_cp = malloc(sizeof(dog_t));
-	if (ptr == NULL || ptr_cp == NULL)
+	char *strn;
+	char *strw;
+	dog_t *ptr;
+
+	strn = cp(name);
+	strw = cp(owner);
+	ptr = malloc(sizeof(dog_t));
+	if (ptr == NULL || strn == NULL || strw == NULL)
 		return (NULL);
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
 
-	ptr_cp->name = name;
-	ptr_cp->owner = owner;
-
 	return (ptr);
+}
+
+/**
+  * cp - duplicate str into a new memory location
+  * @str: the string to be copied
+  *
+  * Return: Always a pointer, else NULL
+  */
+char *cp(char *str)
+{
+	int i;
+	char *p;
+
+	if (str != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+
+		p = malloc((i + 1) * sizeof(char));
+		if (p != NULL)
+		{
+			i = 0;
+			while (*(str + i) != '\0')
+			{
+				p[i] = *(str + i);
+				i++;
+			}
+			p[i] = '\0';
+			return (p);
+		}
+	}
+	return (NULL);
 }

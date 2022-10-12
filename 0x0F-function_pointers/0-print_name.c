aa0e2ee836f762d80d40bac2fd74b29d7f06aa18
Elisha Bemjamin <malishben360@gmail.com>
2022-10-12 14:12:12 -0700

    Task 0 0x0F LLP

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - peints string on the standard output
+  * @name: the string to be printed
+  * @f: the call-back funtion to be invoked
+  *
+  * Return: nothing.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+#INTRIDUCTION TO FUNCTION POINTER
+
+**0-print_name**
+using a call-back fubction to print a string.

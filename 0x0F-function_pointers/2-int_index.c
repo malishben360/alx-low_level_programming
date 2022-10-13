81af5f67079c51b250dda51c0c8eaa46a07af6f1
Elisha Bemjamin <malishben360@gmail.com>
2022-10-12 21:46:53 -0700

    Task 2 0x0F LLP

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+  * int_index - search for an integer in an array
+  * @array: the the list to tranverse through
+  * @size: the size of the list
+  * @cmp: the call-back fumction for comparing items
+  *
+  * Return: the index of an element, else -1 (Failure).
+  */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+	int i;
+
+	if (!(size <= 0))
+	{
+		for (i = 0; i < size; i++)
+		{
+			index = (*cmp)(array[i]);
+			if (index != 0)
+			{
+				return (i);
+			}
+		}
+		return (-1);
+	}
+	return (-1);
+}

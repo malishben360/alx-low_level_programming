92c7505dc539031895fa3abc403ccd8a829e9679
Elisha Bemjamin <malishben360@gmail.com>
2022-10-16 10:50:07 -0700

    Task 0 0x10 LLP

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+  * sum_them_all - sum up all the variable arguments passed
+  * @n: number of the supplied arguments excluded
+  *
+  * Return: the sum of all the arguments (Success) else 0 (Failure).
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	va_list ptr;
+
+	sum = 0;
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,7 @@
+#INTRODUCTION TO VARIADIC FUNCTIONS
+# how to create a variadic function
+# how to access its arguments
+# the basic MACROs for variadic fumctions
+
+** 0-sum_them_all.c **
+ sums all the variable arguments passed.

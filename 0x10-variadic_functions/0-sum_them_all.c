6e7f299ce8ceef5fa8eb8a601a24d6c9fe86fcfb
Elisha Bemjamin <malishben360@gmail.com>
2022-10-16 11:06:02 -0700

    Task 0 0x10 LLP

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,13 +13,16 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	va_list ptr;
 
-	sum = 0;
-	va_start(ptr, n);
-	for (i = 0; i < n; i++)
+	if (n != 0)
 	{
-		sum += va_arg(ptr, int);
+		sum = 0;
+		va_start(ptr, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(ptr, int);
+		}
+		va_end(ptr);
+		return (sum);
 	}
-	va_end(ptr);
-
-	return (sum);
+	return (0);
 }

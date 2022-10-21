0e94c473f0e8ca9df32e97d44cb5dfe3ff8ddfe7
Elisha Bemjamin <malishben360@gmail.com>
2022-10-21 12:11:28 -0700

    Task 2 0x12 LLP

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+  * add_node - inserts a new node at the begging of a list
+  * @head: head of the list
+  * @str: data of the new node
+  *
+  * Return: Pointer to the new node(Success),
+  * else NULL (FAIL).
+  */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	char *ptr = "";
+	int len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	if (str != NULL && str[0] != '\0')
+	{
+		len = strlen(str);
+		ptr = strdup(str);
+	}
+	new->str = ptr;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}

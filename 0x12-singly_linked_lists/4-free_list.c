c381b5dce0822b8012fe48a6282d74801c389d40
Elisha Bemjamin <malishben360@gmail.com>
2022-10-21 15:56:25 -0700

    Task 4 0x12 LLP

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+  * free_list - frees all the nodes of a given list
+  * from the heap
+  * @head: the first node of the list
+  *
+  * Return: nothing.
+  */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (!head)
+		exit(98);
+	temp = head;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}

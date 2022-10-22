b10de962d59f94fc51e29f2ddde71a838228af65
Elisha Bemjamin <malishben360@gmail.com>
2022-10-22 01:36:21 -0700

    Task 100 0x12 LLP

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+  * first - prints a strings on the stdout before the main method
+  *
+  * Return: nothing.
+  */
+void first(void)
+{
+	printf("You're beat! and yet, you most allow,\n");
+	printf("I bore my house upon my back!\n");
+}

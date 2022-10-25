4e3526451287cd585f6ed413d736ac97658e9bc9
Elisha Bemjamin <malishben360@gmail.com>
2022-10-25 05:51:33 -0700

    Task 9 0x13 LLP

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,20 +13,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *new, *lptr, *rptr;
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
-	if ((head == NULL || *head == NULL) && idx == 0)
-	{
-		*head = new;
-		return (new);
-	}
-	if ((head == NULL || *head == NULL) && idx > 0)
-	{
-		free(new), new = NULL;
-		return (NULL);
-	}
 
 	new->n = n, new->next = NULL;
 	rptr = *head, lptr = NULL;

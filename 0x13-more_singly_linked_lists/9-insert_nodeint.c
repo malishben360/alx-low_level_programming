d8fd45b9fd6e6e4dc45e6d9ee3e71fe040bf0bc3
Elisha Bemjamin <malishben360@gmail.com>
2022-10-25 04:35:41 -0700

    Task 9 0x13 LLP

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+  * insert_nodeint_at_index - inserts a new node at a given position
+  * @head: pointer to the first node
+  * @idx: position of the new node
+  * @n: data of the new node
+  *
+  * Return: Pointer to the new node, or NULL if insert failed.
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *new, *lptr, *rptr;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	if ((head == NULL || *head == NULL) && idx == 0)
+	{
+		*head = new;
+		return (new);
+	}
+	if ((head == NULL || *head == NULL) && idx > 0)
+	{
+		free(new), new = NULL;
+		return (NULL);
+	}
+
+	new->n = n, new->next = NULL;
+	rptr = *head, lptr = NULL;
+	while (rptr != NULL)
+	{
+		if (i == idx && lptr != NULL)
+		{
+			new->next = rptr, lptr->next = new;
+			lptr = NULL, rptr = NULL;
+			return (new);
+		}
+		if (i == idx && lptr == NULL)
+		{
+			new->next = *head, *head = new;
+			lptr = NULL, rptr = NULL;
+			return (new);
+		}
+		i++;
+		lptr = rptr;
+		rptr = rptr->next;
+	}
+	free(new), new = NULL, lptr = NULL, rptr = NULL;
+	return (NULL);
+}

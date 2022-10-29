1b67f86e02d9706025c52cddb0fa788b268b853c
Elisha Bemjamin <malishben360@gmail.com>
2022-10-28 23:03:31 -0700

    Task 1 0x14 LLP

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * print_binary - prints the binary represention of a decimal number
+  * starting from the left most significant bit
+  * @n: decimal number
+  *
+  * Return: nothing.
+  */
+void print_binary(unsigned long int n)
+{
+	int i, flag;
+
+	flag = 0;
+	for (i = 31; i >= 0; i--)
+	{
+		if ((n & (1 << i)) != 0)
+		{
+			_putchar(49);
+			flag = 1;
+		}
+		else if (flag == 1)
+		{
+			_putchar(48);
+		}
+	}
+	if (n == 0)
+		_putchar(48);
+	_putchar(10);
+}

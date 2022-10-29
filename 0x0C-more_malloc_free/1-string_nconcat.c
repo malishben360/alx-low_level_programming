822ddb4834c92b1e766e9f95a3ae92efc8e54e37
Elisha Bemjamin <malishben360@gmail.com>
2022-10-29 05:16:36 -0700

    Task 1 0x0C LLP

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,18 +11,22 @@
   */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, n1, n2;
+	unsigned int i = 0, j = 0, n1 = 0, n2 = 0;
 	char *ptr;
 
-	for (n1 = 0; s1[n1] != '\0'; n1++)
+	if (s1 != NULL)
+	for (; s1[n1] != '\0'; n1++)
 		;
-	for (n2 = 0; s2[n2] != '\0'; n2++)
+	if (s2 != NULL)
+	for (; s2[n2] != '\0'; n2++)
 		;
 	ptr = malloc(sizeof(char) * (n1 + n2 + 1));
 	if (ptr == NULL)
 		return (NULL);
+	if (s1 != NULL)
 	for (i = 0; i < n1; i++)
 		ptr[i] = s1[i];
+	if (s2 != NULL)
 	for (n1 = i, j = 0; i < (n2 + n1) && i < (n + n1); i++, j++)
 		ptr[i] = s2[j];
 	ptr[i] = '\0';

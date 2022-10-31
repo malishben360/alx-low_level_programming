a5a1ce093419437f30a3d145cf26b95092ef36e9
Elisha Bemjamin <malishben360@gmail.com>
2022-10-31 01:25:01 -0700

    Task 2 0x14 LLP

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * get_bit - search for a bit at a given index
+  * @n: decimal number to search through
+  * @index: position of the bit in binary rep of n
+  *
+  * Return: Bit 0/1 (success), or -1 if failed.
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((n & (1 << index)) == 0)
+		return (0);
+	else if ((n & (1 << index)) > 0)
+		return (1);
+	return (-1);
+}

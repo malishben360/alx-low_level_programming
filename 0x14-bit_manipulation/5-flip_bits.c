74a3d25c1ba4de7cbb522375d614cee5e9c5445a
Elisha Bemjamin <malishben360@gmail.com>
2022-10-31 06:08:07 -0700

    Task 5 0x14 LLP

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * flip_bits - computes the number of bits to flip to
+  * get from a number to another
+  * @n: number from
+  * @m: number to
+  *
+  * Return: Always number of bits to flip.
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i;
+	unsigned int count = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if ((n & (1 << i)) != (m & (1 << i)))
+			count += 1;
+	}
+	return (count >> 1);
+}

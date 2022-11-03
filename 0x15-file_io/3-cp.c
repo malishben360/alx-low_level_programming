2341c53a6965f8ee3aaf8222636f6eb7504f037d
Elisha Bemjamin <malishben360@gmail.com>
2022-11-03 01:20:20 -0700

    Task 3 0x15 LLP

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+  * main - copy the content of a file to another file
+  * @argc: number of arguments passed
+  * @argv: array of arguments passed
+  *
+  * Return: 98 (Success), or if failed (97, 99, 100).
+  */
+int main(int argc, char *argv[])
+{
+	int fd_0, fd_1, fd_from, fd_to, read_bytes, write_bytes;
+	char *buf;
+
+	if (argc < 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+		exit(98);
+	fd_from = open(argv[1], O_RDONLY);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	read_bytes = read(fd_from, buf, 1024);
+	if (fd_from == -1 || read_bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]); 
+		close(fd_from), close(fd_to);
+		exit(98);
+	}
+	write_bytes = write(fd_to, buf, read_bytes);
+	if (write_bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from), close(fd_to);
+		exit(99);
+	}
+	fd_0 = close(fd_from), fd_1 = close(fd_to);
+	if (fd_0 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_0);
+		exit(100);
+	}
+	if (fd_1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_1);
+		exit(100);
+	}
+	free(buf);
+	exit(98);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,7 @@
 
 /* import libraries */
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>

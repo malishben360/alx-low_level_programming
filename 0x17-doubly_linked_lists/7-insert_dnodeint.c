b547d5d66b2ec626c85ff34fe5c28397565cc138
Elisha Bemjamin <malishben360@gmail.com>
2022-12-13 06:10:25 -0800

    Task 7 0x17 LLP

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,43 +10,44 @@
   */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int len;
-	dlistint_t *node, *tmp1, *tmp2;
+	dlistint_t *temp, *temp2, *new_node;
+	unsigned int i = 0;
 
 	if (h == NULL)
 		return (NULL);
-
-	if (!(n < 0))
+	if (idx != 0)
 	{
-		tmp1 = *h, len = 0;
-		while (tmp1 && len != idx)
+		temp = *h;
+		while (i < (idx - 1) && temp != NULL)
 		{
-			len++, tmp1 = tmp1->next;
+			temp = temp->next;
+			i++;
 		}
-		if (tmp1 == NULL)
+		if (temp == NULL)
 			return (NULL);
 	}
-	else
-	{
-		return (NULL);
-	}
 
-	node = malloc(sizeof(dlistint_t));
-	if (node == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	node->n = n;
-	if (n == 0)
+	new_node->n = n;
+	if (idx == 0)
 	{
-		tmp2 = *h, node->prev = NULL, node->next = tmp2;
-		if (tmp2 != NULL)
-			tmp2->prev = node;
-		*h = node;
+		temp2 = *h;
+		*h = new_node;
+		new_node->prev = NULL;
+		new_node->next = temp2;
 	}
 	else
 	{
-		node->prev = tmp1->prev, node->next = tmp1;
-		tmp1->prev = node, node->prev->next = node;
+		new_node->prev = temp;
+		temp2 = temp->next;
+		temp->next = new_node;
+		new_node->next = temp2;
 	}
-	return (node);
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
+	return (new_node);
 }
+
